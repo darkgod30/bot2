82b241f4de8537935e81958a6904e21fd362a99e
darkgod30 <81325272+darkgod30@users.noreply.github.com>
2021-03-26 12:07:31 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,13 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/557799922868 ou digite *${prefix}owner*_
